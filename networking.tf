3dae8fd87a6fda293a47c8336b7447757fb47c42
Triston <tristonosborn2002@gmail.com>
2024-06-08 15:00:17 -0700

    Hopefully final push

--- networking.tf
+++ networking.tf
@@ -1,28 +1,15 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "minecraft-vpc"
-  }
 }
 
 resource "aws_subnet" "subnet" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-west-2b"  
-  map_public_ip_on_launch = true  
-
-  tags = {
-    Name = "minecraft-subnet"
-  }
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "minecraft-igw"
-  }
 }
 
 resource "aws_route_table" "route_table" {
@@ -31,10 +18,6 @@ resource "aws_route_table" "route_table" {
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "minecraft-route-table"
   }
 }
 
@@ -47,7 +30,7 @@ resource "aws_security_group" "minecraft_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 25565
+    from_port   = 22
     to_port     = 25565
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
@@ -58,9 +41,5 @@ resource "aws_security_group" "minecraft_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "minecraft-sg"
   }
 }
